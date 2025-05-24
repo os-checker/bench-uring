541e4b5cb04a4a8fe513df9302fcabf77110d720
zjp <jiping_zhou@foxmail.com>
2025-05-24 03:12:06 +0000

    main.rs: accept the first argument as csv path
    and default to data.csv if the argument is absent

--- src/main.rs
+++ src/main.rs
@@ -7,12 +7,16 @@ extern crate tracing;
 fn main() -> bench_uring::Result {
     bench_uring::logger::init();
 
+    let args: Vec<_> = std::env::args().collect();
+    // The first argument is a path to csv.
+    let csv = args.get(1).map(|s| s.as_str()).unwrap_or("data.csv");
+
     let examples = Examples::new()?;
 
     examples.build()?;
     let throughputs = examples.bench()?;
     dbg!(&throughputs);
-    write_csv("data.csv", &throughputs)?;
+    write_csv(csv, &throughputs)?;
 
     Ok(())
 }
